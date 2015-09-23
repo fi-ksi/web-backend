efeb59f1d7b623c34ed3ef91fede0be90f65b81f
Martin Polednik <mpolednik@redhat.com>
2015-09-23 22:08:07 +0200

    Add support for RFC6749 refresh tokens

--- endpoint/oauth2.py
+++ endpoint/oauth2.py
@@ -1,15 +1,15 @@
 import auth
 
 
-class Auth(object):
+class Authorize(object):
     provider = auth.Provider()
 
     def on_post(self, req, resp):
         self.provider.request_access_token(req, resp)
 
 
-class Token(object):
+class Refresh(object):
     provider = auth.Provider()
 
     def on_get(self, req, resp):
-        self.provider.access_token_request(req, resp)
+        self.provider.refresh_access_token(req, resp)

684fe6f45eb8e6adbcbab4d9748bc8b3340acd03
Jan Stourac <stourac.jan@gmail.com>
2015-09-24 22:33:50 +0200

    Docasna data pro uzivatele & profil
    Prihlasovani funguje na zaklade fake hlavicek.

--- endpoint/profile.py
+++ endpoint/profile.py
@@ -1,8 +1,19 @@
-class Profile(object):
-    def _schema(self, req):
-        return {'profile': [
-            {'id': 1, 'is_logged': bool(req.env['PERMISSIONS'])}
-        ]}
+import json, falcon
 
-    def on_get(self, req, resp):
-        req.context['result'] = self._schema(req)
+class Profile(object):
+	def _schema(self, req):
+		return {'profile': [
+			{'id': 1, 'is_logged': bool(req.env['PERMISSIONS'])}
+		]}
+
+	def on_options(self, req, resp):
+		resp.set_header('Access-Control-Allow-Credentials', 'true')
+		resp.set_header('Access-Control-Allow-Headers', 'authorization')
+		resp.set_header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+
+		#resp.status = falcon.HTTP_204
+
+	def on_get(self, req, resp):
+		#req.context['result'] = self._schema(req)
+		resp.set_header('X-Powered-By', 'Small Furry Creatures')
+		req.context['result'] =  json.loads(' '.join(open('/tmp/profil.json').readlines()))
--- endpoint/user.py
+++ endpoint/user.py
@@ -3,26 +3,104 @@ import model
 
 
 class User(object):
-    def _schema(self, model_instances):
-        inst = model_instances
+	def _schema(self, model_instances):
+		inst = model_instances
 
-        return {'user':
-                {'id': inst.id, 'first_name': inst.name_first,
-                 'last_name': inst.name_last}
-                }
+		return {'user':
+				{'id': inst.id, 'first_name': inst.name_first,
+				 'last_name': inst.name_last}
+				}
 
-    def on_get(self, req, resp, id):
-        user = session.query(model.User).get(id)
-        req.context['result'] = self._schema(user)
+	def on_get(self, req, resp, id):
+		#user = session.query(model.User).get(id)
+		req.context['result'] = { 'user':  {"id": 1,
+	  "first_name": "Katka",
+	  "last_name": "Skvela",
+	  "score": 150,
+	  "tasks_num": 16,
+	  "profile_picture": "http://placehold.it/50x50",
+	  "achievements": [
+		1,
+		2,
+		3
+	  ]
+	  } }
 
 
 class Users(object):
-    def _schema(self, model_instances):
-        return {'users': [
-            {'id': inst.id, 'first_name': inst.name_first,
-             'last_name': inst.name_last} for inst in model_instances
-        ]}
+	def _schema(self, model_instances):
+		return {'users': [
+			{'id': inst.id, 'first_name': inst.name_first,
+			 'last_name': inst.name_last} for inst in model_instances
+		]}
 
-    def on_get(self, req, resp):
-        users = session.query(model.User).all()
-        req.context['result'] = self._schema(users)
+	def on_get(self, req, resp):
+		#users = session.query(model.User).all()
+		req.context['result'] = { "users": [
+	{
+	  "id": 1,
+	  "first_name": "Katka",
+	  "last_name": "Skvela",
+	  "score": 150,
+	  "tasks_num": 16,
+	  "profile_picture": "http://placehold.it/50x50",
+	  "achievements": [
+		1,
+		2,
+		3
+	  ]
+	},
+	{
+	  "id": 2,
+	  "first_name": "Katka",
+	  "last_name": "Skvela",
+	  "score": 150,
+	  "tasks_num": 16,
+	  "achievements": [
+		2,
+		3
+	  ]
+	},
+	{
+	  "id": 3,
+	  "first_name": "Katka",
+	  "last_name": "Skvela",
+	  "score": 150,
+	  "tasks_num": 16,
+	  "achievements": [
+		1,
+		2,
+		3
+	  ]
+	},
+	{
+	  "id": 4,
+	  "first_name": "Katka",
+	  "last_name": "Skvela",
+	  "score": 150,
+	  "tasks_num": 16,
+	  "achievements": [
+		2
+	  ]
+	},
+	{
+	  "id": 5,
+	  "first_name": "Katka",
+	  "last_name": "Skvela",
+	  "score": 150,
+	  "tasks_num": 16,
+	  "achievements": [
+		1,
+		3
+	  ]
+	},
+	{
+	  "id": 6,
+	  "first_name": "Jan",
+	  "last_name": "Horacek",
+	  "score": 1,
+	  "tasks_num": 1,
+	  "achievements": []
+	}
+  ]
+}

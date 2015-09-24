e329c48d171e48adca3a54852a10c243d1c5b347
Jan Stourac <stourac.jan@gmail.com>
2015-09-24 22:24:48 +0200

    Docasne zakomentovani neimplementovanych modelu

--- model/__init__.py
+++ model/__init__.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 from model.article import Article
-from model.achievement import Achievement
-from model.category import Category
+#~ from model.achievement import Achievement
+#~ from model.category import Category
 from model.post import Post
-from model.task import Task
+#~ from model.task import Task
 from model.thread import Thread
-from model.user import User
-from model.auth import Token
+#~ from model.user import User
+#~ from model.auth import Token

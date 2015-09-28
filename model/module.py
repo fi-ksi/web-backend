c6fb93260eb8918184f5cddc747e3248d0ff7597
Jan Stourac <stourac.jan@gmail.com>
2015-09-28 13:58:14 +0200

    Kazda uloha muze mit 0-n kazdeho typu modulu

--- model/module.py
+++ model/module.py
@@ -1,15 +1,13 @@
-from sqlalchemy import Column, Integer, SmallInteger, String, Text, Enum, ForeignKey, UniqueConstraint
+from sqlalchemy import Column, Integer, SmallInteger, String, Text, Enum, ForeignKey
 
 from . import Base
 
 class Module(Base):
 	__tablename__ = 'modules'
-	__table_args__ = (
-		UniqueConstraint('task', 'type', name='_task_type_uc'),
-		{
-			'mysql_engine': 'InnoDB',
-			'mysql_charset': 'utf8',
-		})
+	__table_args__ = {
+		'mysql_engine': 'InnoDB',
+		'mysql_charset': 'utf8',
+	}
 
 	id = Column(Integer, primary_key=True)
 	task = Column(Integer, ForeignKey('tasks.id'), nullable=False)

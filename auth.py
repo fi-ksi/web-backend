c24437e78b849d1e42673d89e1a6a4cfd54fab5c
Jan Stourac <stourac.jan@gmail.com>
2015-09-29 16:42:40 +0200

    REFACTOR: Formatovani auth.py

--- auth.py
+++ auth.py
@@ -1,52 +1,44 @@
-import string
-import random
-import urllib
-
-import falcon
+import string, random, urllib, falcon
 
 from db import session
 import model
 
 TOKEN_LENGTH = 40
 
-
 def _generate_token():
-    return ''.join([random.choice((string.ascii_letters +
-                                   string.digits)) for
-                    x in range(TOKEN_LENGTH)])
-
+	return ''.join( [ random.choice((string.ascii_letters + string.digits)) for x in range(TOKEN_LENGTH) ] )
 
 class Error:
-    INVALID_REQUEST = 'invalid_request'
-    UNAUTHORIZED_CLIENT = 'unauthorized_client'
+	INVALID_REQUEST = 'invalid_request'
+	UNAUTHORIZED_CLIENT = 'unauthorized_client'
 
 
 class GrantType:
-    CODE = 'password'
-    CODE = 'refresh_token'
+	CODE = 'password'
+	CODE = 'refresh_token'
 
 
 class OAuth2Token(object):
-    def __init__(self, client_id):
-        self.value = _generate_token()
-        self.expire = 30
-        self.kind = 'Bearer'
-        self.refresh = _generate_token()
+	def __init__(self, client_id):
+		self.value = _generate_token()
+		self.expire = 30
+		self.kind = 'Bearer'
+		self.refresh = _generate_token()
 
-        token = model.Token()
-        token.access_token = self.value
-        token.expire = self.expire
-        token.refresh_token = self.refresh
-        token.user = client_id
+		token = model.Token()
+		token.access_token = self.value
+		token.expire = self.expire
+		token.refresh_token = self.refresh
+		token.user = client_id
 
-        session.add(token)
-        session.commit()
+		session.add(token)
+		session.commit()
 
-    @property
-    def data(self):
-        return {
-            'access_token': self.value,
-            'token_type': self.kind,
-            'expires_in': self.expire,
-            'refresh_token': self.refresh
-        }
+	@property
+	def data(self):
+		return {
+			'access_token': self.value,
+			'token_type': self.kind,
+			'expires_in': self.expire,
+			'refresh_token': self.refresh
+		}

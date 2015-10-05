2775fccac2a9d39d3fc41c0169696a4495856c7c
Jan Stourac <stourac.jan@gmail.com>
2015-10-05 15:16:33 +0200

    Zprovozneni ukladani vsech dat pri registraci

--- endpoint/registration.py
+++ endpoint/registration.py
@@ -9,17 +9,16 @@ class Registration(object):
 	def on_options(self, req, resp):
 		return
 
-	#TODO: Realne ukladani dat
 	def on_post(self, req, resp):
 		data = json.loads(req.stream.read())
 
-		user = model.User(email=data['email'], password=auth.get_hashed_password(data['password']), first_name=data['first_name'], last_name=data['last_name'], sex='male', short_info=data["short_info"])
+		user = model.User(email=data['email'], password=auth.get_hashed_password(data['password']), first_name=data['first_name'], last_name=data['last_name'], sex=data['gender'], short_info=data["short_info"])
 		session.add(user)
 		session.commit()
 
-		profile = model.Profile(user_id=user.id, addr_street=data['addr_street'], addr_city=data['addr_city'], addr_zip=data['addr_zip'], addr_country='cz',\
-			school_name=data['school_name'], school_street=data['school_street'], school_city=data['school_city'], school_zip=data['school_zip'], school_country='cz', school_finish=int(data['school_finish']),\
-			tshirt_size='S')
+		profile = model.Profile(user_id=user.id, addr_street=data['addr_street'], addr_city=data['addr_city'], addr_zip=data['addr_zip'], addr_country=data['addr_country'],\
+			school_name=data['school_name'], school_street=data['school_street'], school_city=data['school_city'], school_zip=data['school_zip'], school_country=data['school_country'], school_finish=int(data['school_finish']),\
+			tshirt_size=data['tshirt_size'].upper())
 		session.add(profile)
 
 		session.commit()

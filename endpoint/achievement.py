085b970e8373ddfc8872afe81282cd18be236159
Jan Stourac <stourac.jan@gmail.com>
2015-10-05 12:47:02 +0200

    Opraveno nacitacni spravnych obrazku pro achievementy

--- endpoint/achievement.py
+++ endpoint/achievement.py
@@ -5,14 +5,16 @@ import util
 class Achievement(object):
 
 	def on_get(self, req, resp, id):
+		user = req.context['user']
 		achievement = session.query(model.Achievement).get(id)
 
-		req.context['result'] = { 'achievement': util.achievement.to_json(achievement) }
+		req.context['result'] = { 'achievement': util.achievement.to_json(achievement, user.id) }
 
 
 class Achievements(object):
 
 	def on_get(self, req, resp):
+		user = req.context['user']
 		achievements = session.query(model.Achievement).all()
 
-		req.context['result'] = { 'achievements': [ util.achievement.to_json(achievement) for achievement in achievements ] }
+		req.context['result'] = { 'achievements': [ util.achievement.to_json(achievement, user.id) for achievement in achievements ] }
--- util/achievement.py
+++ util/achievement.py
@@ -1,6 +1,10 @@
+from db import session
+import model
 
-def to_json(achievement):
-	return { 'id': achievement.id, 'title': achievement.title, 'active': True, 'picture_active': 'img/achievements/' + achievement.code + '.svg' }
+def to_json(achievement, user_id):
+	show_picture = user_id and session.query(model.UserAchievement).filter(model.UserAchievement.achievement_id == achievement.id, model.UserAchievement.user_id == user_id).first() is not None
+
+	return { 'id': achievement.id, 'title': achievement.title, 'active': True, 'picture': '/img/achievements/' + (achievement.code if show_picture else 'achievement-unknown') + '.svg' }
 
 def ids_set(achievements):
 	return set([ achievement.id for achievement in achievements ])

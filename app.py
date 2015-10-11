40d86ab02c3c5d0e9b6509dedbae505fc5aadfda
Jan Mrázek <email@honzamrazek.cz>
2015-10-11 13:17:22 +0200

    Add new CORS sources

--- app.py
+++ app.py
@@ -58,7 +58,9 @@ def cors_middleware(request, response, params):
 
 	if origin in ('http://localhost:4200',
 				  'http://147.251.43.191',
-				  'http://ksi.fi.muni.cz'):
+				  'http://ksi.fi.muni.cz',
+				  'https://147.251.43.191'
+				  'https://ksi.fi.muni.cz'):
 						response.set_header('Access-Control-Allow-Origin', origin)
 
 	response.set_header('Access-Control-Allow-Headers', 'authorization,content-type')

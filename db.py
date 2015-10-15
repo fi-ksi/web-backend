405298239e31465d43e0636fa3eadde468f430f4
Jan Horacek <me@apophis.cz>
2015-10-15 20:31:06 +0200

    SQL change is propagated through sqlalchemy to frontend.
    Till now, when sql changed, sqlalchemy did not propagate the change. This bug is now fixed.

--- db.py
+++ db.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 import config
 
-engine = sqlalchemy.create_engine(config.SQL_ALCHEMY_URI)
+engine = sqlalchemy.create_engine(config.SQL_ALCHEMY_URI, isolation_level="READ COMMITTED")
 _session = sessionmaker(bind=engine)
 session = _session()

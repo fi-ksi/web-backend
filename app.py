349c5e5f6b6d91a24ba2c4e00fc27a3a97fa8d50
KSI Web <ksi@ksi2.fi.muni.cz>
2015-10-20 13:58:29 +0200

    Fix missing comma

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def cors_middleware(request, response, params):
 	if origin in ('http://localhost:4200',
 				  'http://147.251.43.191',
 				  'http://ksi.fi.muni.cz',
-				  'https://147.251.43.191'
+				  'https://147.251.43.191',
 				  'https://ksi.fi.muni.cz'):
 						response.set_header('Access-Control-Allow-Origin', origin)
 

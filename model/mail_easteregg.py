8a7fad474689045e0777abd86f8a42ea26779fdf
Eva <smijakova.eva@gmail.com>
2015-11-01 00:59:22 +0100

    models for config & mail_easter egg + corrected new lines for mails

---
+++ model/mail_easteregg.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, Text
+
+from . import Base
+
+class MailEasterEgg(Base):
+	__tablename__ = 'mail_easteregg'
+	__table_args__ = {
+		'mysql_engine': 'InnoDB',
+		'mysql_charset': 'utf8'
+	}
+
+	id = Column(Integer, primary_key=True, nullable=False)
+	body = Column(Text, nullable=False)

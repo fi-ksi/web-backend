ed0c3ccda7083ba8c685ac82594b94c908a0fb57
Dev Server <root@kyzikos.fi.muni.cz>
2015-11-01 18:15:59 +0100

    maily pre obecnu diskusiu a k uloham, mailove eastereggy

--- model/config.py
+++ model/config.py
@@ -2,12 +2,13 @@ from sqlalchemy import Column, String
 
 from . import Base
 
-class MailEasterEgg(Base):
-	__tablename__ = 'mail_easteregg'
+class Config(Base):
+	__tablename__ = 'config'
 	__table_args__ = {
 		'mysql_engine': 'InnoDB',
 		'mysql_charset': 'utf8'
 	}
 
-	id = Column(String(100), primary_key=True, nullable=False)
-	body = Column(String(200), nullable=False)
+	key = Column(String(100), primary_key=True, nullable=False)
+	value = Column(String(200), nullable=False)
+

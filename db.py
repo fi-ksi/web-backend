eaf8dcbbfa7de92832f7fed394e7b95ddf65e8b1
Jan Horacek <me@apophis.cz>
2015-11-01 11:24:47 +0100

    Update sqalchemy db timeout.

--- db.py
+++ db.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 import config
 
-engine = sqlalchemy.create_engine(config.SQL_ALCHEMY_URI, isolation_level="READ COMMITTED", pool_recycle=-1)
+engine = sqlalchemy.create_engine(config.SQL_ALCHEMY_URI, isolation_level="READ COMMITTED", pool_recycle=3600)
 _session = sessionmaker(bind=engine)
 session = _session()

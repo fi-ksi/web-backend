9760d012a4692aa7697fb1d781d4f85b54daa8f2
Jan Horacek <me@apophis.cz>
2015-11-04 17:31:13 +0100

    Add achievement loading from backend.

--- util/achievement.py
+++ util/achievement.py
@@ -1,8 +1,9 @@
 from db import session
 import model
+from util import config
 
 def to_json(achievement, user_id):
-	return { 'id': achievement.id, 'title': achievement.title, 'active': True, 'picture': '/img/achievements/' + achievement.code + '.svg', 'description': achievement.description }
+	return { 'id': achievement.id, 'title': achievement.title, 'active': True, 'picture': config.KSI_WEB+':3000/content/achievements/' + achievement.code + '.svg', 'description': achievement.description }
 
 def ids_set(achievements):
 	return set([ achievement.id for achievement in achievements ])

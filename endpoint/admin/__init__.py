6ce9d7b429d0f45168f3e5163a6217d948ef6bec
Jan Horacek <me@apophis.cz>
2015-11-15 21:20:40 +0100

    Add zip submission endpoints.

--- endpoint/admin/__init__.py
+++ endpoint/admin/__init__.py
@@ -1,2 +1,6 @@
 from endpoint.admin.corrections import Corrections
+from endpoint.admin.corrections import Correction
 from endpoint.admin.correctionsInfo import CorrectionsInfo
+from endpoint.admin.correctionsPublish import CorrectionsPublish
+from endpoint.admin.submFilesEval import SubmFilesEval
+from endpoint.admin.submFilesTask import SubmFilesTask

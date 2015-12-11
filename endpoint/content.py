6d449884f88425f8687d530f9e269897c12185f4
Jan Horacek <me@apophis.cz>
2015-12-11 11:17:37 +0100

    Remove task-content redundant queries.

--- endpoint/content.py
+++ endpoint/content.py
@@ -25,30 +25,9 @@ class TaskContent(object):
 	def on_get(self, req, resp, id, view):
 		user = req.context['user']
 
-		task = session.query(model.Task).get(id)
-
-		if task is None:
-			resp.status = falcon.HTTP_400
-			return
-
 		if not view in ['zadani', 'reseni', 'icon']:
 			resp.status = falcon.HTTP_400
 			return
-		"""
-		status = util.task.status(task, user)
-
-		if view == 'zadani':
-			if status == util.TaskStatus.LOCKED:
-				resp.status = falcon.HTTP_403
-				return
-		elif view == 'reseni':
-			if not util.task.solution_public(status, task, user):
-				resp.status = falcon.HTTP_403
-				return
-		elif view != 'icon':
-			resp.status = falcon.HTTP_400
-			return
-		"""
 
 		filePath = 'data/task-content/' + id + '/' + view + '/' + req.get_param('path').replace('..', '')
 

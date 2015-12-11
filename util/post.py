c94a4d28785733e1d0042545ff7fde81d7a4385d
Jan Horacek <me@apophis.cz>
2015-12-11 10:42:12 +0100

    Fix post` to_json

--- util/post.py
+++ util/post.py
@@ -1,12 +1,13 @@
 from db import session
 import model
 
-def to_json(post, user_id, last_visit=None):
+def to_json(post, user_id, last_visit=None, last_visit_filled=False):
 	if user_id:
-		if not last_visit:
+		if not last_visit_filled:
 			last_visit = session.query(model.ThreadVisit).\
 				filter(model.ThreadVisit.user == user_id, model.ThreadVisit.thread == post.thread, model.ThreadVisit.last_last_visit.isnot(None)).first()
-		is_new = True if not last_visit else last_visit.last_last_visit < post.published_at
+		print last_visit.last_last_visit
+		is_new = True if not last_visit or last_visit.last_last_visit is None else last_visit.last_last_visit < post.published_at
 	else:
 		is_new = False
 

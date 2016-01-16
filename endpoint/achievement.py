300dddc30eb9f44cc864a4214f24d3e478da1a52
Jan Horacek <me@apophis.cz>
2016-01-16 13:52:25 +0100

    Add achievement deleting.

--- endpoint/achievement.py
+++ endpoint/achievement.py
@@ -14,6 +14,35 @@ class Achievement(object):
 
 		req.context['result'] = { 'achievement': util.achievement.to_json(achievement) }
 
+	def on_delete(self, req, resp, id):
+		user = req.context['user']
+		achievement = session.query(model.Achievement).get(id)
+
+		if (not user.is_logged_in()) or (not user.is_admin()):
+			resp.status = falcon.HTTP_400
+			return
+
+		if not achievement:
+			resp.status = falcon.HTTP_404
+			return
+
+		# Ziskame vsechna prideleni daneho achievementu
+		user_achs = session.query(model.UserAchievement).\
+			filter(model.UserAchievement.achievement_id == id).all()
+
+		try:
+				for user_ach in user_achs:
+					session.delete(user_ach)
+				session.delete(achievement)
+				session.commit()
+		except:
+				session.rollback()
+				raise
+		finally:
+				session.close()
+
+		req.context['result'] = {}
+
 
 class Achievements(object):
 

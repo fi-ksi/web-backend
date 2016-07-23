581220d46d6568c33148298feb1c21d4184720f5
Jan Horacek <me@apophis.cz>
2016-07-23 22:18:01 +0200

    Fix year import.

--- util/year.py
+++ util/year.py
@@ -3,6 +3,7 @@
 from db import session
 import model
 from util import config
+import util
 
 def to_json(year, sum_points=None):
 	if sum_points is None: sum_points = util.task.max_points_year_dict()[year.id]

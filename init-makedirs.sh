9db4346dc4ca31726950423c85214704821350cd
Jan Horacek <me@apophis.cz>
2016-08-10 20:01:34 +0200

    Add init-makedirs script.

---
+++ init-makedirs.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+# This script makes filesystem structure necessarry for ksi-backend
+# to work. It is intended to be run at (and only at) initialization on
+# a new server. However, if you run this script on running server,
+# it just does nothing.
+
+echo -n "Making data directories..."
+mkdir -p data/code_executions
+mkdir -p data/content/achievements data/content/articles
+mkdir -p data/images
+mkdir -p data/modules
+mkdir -p data/seminar
+mkdir -p data/submissions
+mkdir -p data/task-content
+echo " done"
+
+echo "Do not forget to checkout repository 'seminar' into data/seminar (write access required)."
+echo "Do not forget to checkout repository 'module_lib' into data/module_lib (read access in enough)."

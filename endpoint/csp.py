3b101390a9f06c2f303fb26310c1f86015b2792b
Jan Horacek <me@apophis.cz>
2017-02-28 10:06:21 +0100

    Fix import.

--- endpoint/csp.py
+++ endpoint/csp.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import falcon, util, json
+import falcon, util, json, sys
 
 # Content-security policy reports of frontend
 # Every CSP report is forwarded to ksi-admin@fi.muni.cz.

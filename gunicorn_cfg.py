ee0fc6f0a66beafb93cfbf94d6edf86e4368ba4b
Jan Horacek <me@apophis.cz>
2017-03-06 16:01:33 +0100

    Fix 500 when accessing pure task-content.

--- gunicorn_cfg.py
+++ gunicorn_cfg.py
@@ -12,5 +12,5 @@ def pre_request(worker, req):
     if req.path.startswith('/taskContent/'):
         parts = req.path.split("/")
         req.query = 'path=' + '/'.join(parts[4:])
-        req.path = '/task-content/' + parts[2] + '/' + parts[3]
+        req.path = '/task-content/' + parts[2] + '/' + (parts[3] if len(parts) > 3 else '')
 

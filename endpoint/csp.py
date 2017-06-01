223b61f360c0d0064c0b40d8a5a37490d094bede
Jan Horacek <me@apophis.cz>
2017-06-01 12:07:54 +0200

    Ignore CSP "about" violations.

--- endpoint/csp.py
+++ endpoint/csp.py
@@ -11,6 +11,19 @@ class CSP(object):
 
     def on_post(self, req, resp):
         data = json.loads(req.stream.read())
+
+        # Ignore "about" violations caused by Disconnect plugin
+        if "csp-report" not in data:
+            req.context['result'] = {}
+            resp.status = falcon.HTTP_200
+            return
+
+        if "blokced-uri" in data["csp-report"] and \
+           data["csp-report"]["blocked-uri"] == "about":
+            req.context['result'] = {}
+            resp.status = falcon.HTTP_200
+            return
+
         text = "<p>" + util.config.ksi_web() + \
             "<br><pre>" + json.dumps(data, indent=4) + "</pre></p>" + \
             util.mail.easteregg()

7014fd0a958c66f048c7e4e2f2d4d0644835879a
Jan Horacek <me@apophis.cz>
2017-07-10 10:44:20 +0200

    String must be properly encoded.

--- auth.py
+++ auth.py
@@ -25,7 +25,7 @@ def get_hashed_password(plain_text_password):
     return bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
 
 def check_password(plain_text_password, hashed_password):
-    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 class OAuth2Token(object):
     def __init__(self, client_id):

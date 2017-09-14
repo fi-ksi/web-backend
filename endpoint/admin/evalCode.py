13e271fe12d70d6c32a4c6c9c0094c6d5ccbeb8a
Jan Horacek <me@apophis.cz>
2017-09-14 18:51:36 +0200

    Make evalCode endpoint work.

--- endpoint/admin/evalCode.py
+++ endpoint/admin/evalCode.py
@@ -1,10 +1,19 @@
-# -*- coding: utf-8 -*-
+import falcon
+import os
+from sqlalchemy.exc import SQLAlchemyError
 
 from db import session
-from sqlalchemy.exc import SQLAlchemyError
-import model, os, falcon
+import model
+
 
 class EvalCode(object):
+
+    def _file_or_error(self, fn):
+        if not os.path.isfile(fn):
+            return "Soubor %s neexistuje." % (fn)
+
+        with open(fn, "r") as f:
+            return f.read()
 
     def on_get(self, req, resp, id):
         try:
@@ -18,33 +27,45 @@ class EvalCode(object):
                 filter(model.SubmittedCode.evaluation == id).first()
 
             if not code:
-                req.context['result'] = { 'errors': [ { 'id': 5, 'title': "Code not found in db" } ] }
+                req.context['result'] = {
+                    'errors': [{'id': 5, 'title': "Code not found in db"}]
+                }
                 return
 
             evaluation = session.query(model.Evaluation).get(code.evaluation)
             if not evaluation:
-                req.context['result'] = { 'errors': [ { 'id': 5, 'title': "Evaluation not found in db" } ] }
+                req.context['result'] = {
+                    'errors': [
+                        {'id': 5, 'title': "Evaluation not found in db"}
+                    ]
+                }
                 return
 
-            eval_dir = 'data/submissions/module_' + str(evaluation.module) + '/' + 'user_' + str(evaluation.user) + '/'
+            eval_dir = os.path.join(
+                'data',
+                'exec',
+                'module_' + str(evaluation.module),
+                'user_' + str(evaluation.user))
 
-            try:
-                req.context['result'] = {
-                    'evalCode': {
-                        'id': evaluation.id,
-                        'code': code.code,
-                        'merged': open(eval_dir+'sandbox/code.py', 'r').read() if os.path.isfile(eval_dir+'sandbox/code.py') else "Soubor "+eval_dir+'sandbox/code.py'+" neexistuje",
-                        'stdout': open(eval_dir+'sandbox.stdout').read() if os.path.isfile(eval_dir+'sandbox.stdout') else "Soubor "+eval_dir+'sandbox.stdout'+" neexistuje",
-                        'stderr': open(eval_dir+'sandbox.stderr').read() if os.path.isfile(eval_dir+'sandbox.stderr') else "Soubor "+eval_dir+'sandbox.stderr'+" neexistuje",
-                        'merge_stdout': open(eval_dir+'merge.stdout').read() if os.path.isfile(eval_dir+'merge.stdout') else "Soubor "+eval_dir+'merge.stdout'+" neexistuje",
-                        'check_stdout': open(eval_dir+'check.stdout').read() if os.path.isfile(eval_dir+'check.stdout') else "Soubor "+eval_dir+'check.stdout'+" neexistuje",
-                    }
+            CODE_PATH = os.path.join(eval_dir, 'box', 'run')
+            STDOUT_PATH = os.path.join(eval_dir, 'stdout')
+            STDERR_PATH = os.path.join(eval_dir, 'stderr')
+            MERGE_STDOUT = os.path.join(eval_dir, 'merge.stdout')
+            MERGE_STDERR = os.path.join(eval_dir, 'merge.stderr')
+
+            req.context['result'] = {
+                'evalCode': {
+                    'id': evaluation.id,
+                    'code': code.code,
+                    'merged': self._file_or_error(CODE_PATH),
+                    'stdout': self._file_or_error(STDOUT_PATH),
+                    'stderr': self._file_or_error(STDERR_PATH),
+                    'merge_stdout': self._file_or_error(MERGE_STDOUT),
+                    'check_stdout': self._file_or_error(MERGE_STDERR),
                 }
-            except Exception as e:
-                req.context['result'] = { 'errors': [ { 'id': 10, 'title': str(e) } ] }
+            }
         except SQLAlchemyError:
             session.rollback()
             raise
         finally:
             session.close()
-

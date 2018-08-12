53e9d27eb0d86e664df8c833c2d36b2784f93c2b
Jan Horacek <me@apophis.cz>
2018-08-12 16:24:50 +0200

    Add feedback model.

---
+++ model/feedback.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, ForeignKey, Text
+from sqlalchemy.types import TIMESTAMP
+
+from . import Base
+from .user import User
+from .task import Task
+
+
+class Feedback(Base):
+    __tablename__ = 'feedbacks'
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8mb4'
+    }
+
+    user = Column(
+        Integer,
+        ForeignKey(User.id, ondelete='CASCADE', onupdate='NO ACTION'),
+        primary_key=True,
+        nullable=False,
+    )
+    task = Column(
+        Integer,
+        ForeignKey(Task.id, ondelete='CASCADE', onupdate='NO ACTION'),
+        primary_key=True,
+        nullable=False,
+    )
+    content = Column(Text, nullable=False)
+    lastUpdated = Column(TIMESTAMP, default=datetime.datetime.utcnow,
+                         server_default=text('CURRENT_TIMESTAMP'))

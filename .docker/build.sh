037e130ba7c6850b1d17c5cbba5239e9f9676e80
Adam Hlaváček <git@adamhlavacek.com>
2021-08-27 19:57:31 +0200

    feat(docker): allow autorun after build

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 cd "$(realpath "$(dirname "$0")")/../.." &&
-docker build -f web-backend/.docker/Dockerfile -t ksi-be .
+docker rmi -f ksi-be &&
+docker build -f web-backend/.docker/Dockerfile -t ksi-be . &&
+if [[ "$1" == "--run" ]]; then
+   docker run -p 3030:3030 -v "$2:/var/ksi-be/" -it ksi-be
+fi

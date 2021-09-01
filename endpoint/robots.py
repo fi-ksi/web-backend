6ca47a8caa1ac68cd4d2305f542c81cbecb80f3c
Adam Hlaváček <git@adamhlavacek.com>
2021-09-01 15:00:49 +0200

    fix(robots): add missing HEAD method

--- endpoint/robots.py
+++ endpoint/robots.py
@@ -2,6 +2,9 @@ import falcon
 
 
 class Robots(object):
+    def on_head(self, req, resp):
+        self.on_get(req=req, resp=resp)
+        resp.body = ''
 
     def on_get(self, req, resp):
         resp.content_type = 'text/plain'

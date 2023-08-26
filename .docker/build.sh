5a252547cd706e97e9c8aef91d2b0eea2ac97aa3
Adam Hlaváček <git@adamhlavacek.com>
2023-08-26 16:27:28 +0200

    fix(docker): update to supported version

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(realpath "$(dirname "$0")")/.." &&
 # docker rmi -f ksi-be &>/dev/null &&
-docker build -f .docker/Dockerfile -t ksi-be . &&
+docker build --no-cache -f .docker/Dockerfile -t ksi-be . &&
 if [[ "$1" == "--run" ]]; then
   ./.docker/start.sh "$2" "$3" "$4"
 fi
